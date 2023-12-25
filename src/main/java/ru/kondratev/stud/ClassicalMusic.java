6412008eb9fce5a72615eb824319020e909773d2
AnKondratev <kondratev.an@inbox.ru>
2023-12-25 13:31:26 +0400

    \Spring lesson 12: "Annotations. @Scope, @Value, PostConstruct, @PreDestroy (Dependency injection)"

--- src/main/java/ru/kondratev/stud/ClassicalMusic.java
+++ src/main/java/ru/kondratev/stud/ClassicalMusic.java
@@ -1,18 +1,29 @@
 package ru.kondratev.stud;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Random;
 
 @Component
+@Scope("prototype")
 public class ClassicalMusic implements Music {
 
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my init..");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my dest..");
+    }
     @Override
     public String getSong() {
         Random random = new Random();
-      List<String> list = List.of(
+        List<String> list = List.of(
                 "Classic song_1",
                 "Classic song_2",
                 "Classic song_3"
